9ea6421eeea6a7ba16737b8b26736b5854a201e2
Tim Rolen <trolen@windrock.com>
2016-03-04 03:35:28 +0000

    Make use of regular expressions and iterator to reduce code duplication Add more unit tests

--- day6.py
+++ day6.py
@@ -1,68 +1,52 @@
 #!/usr/bin/env python3
 
 import fileinput
+import re
 
-def get_command(line):
-  if line.startswith('turn off'):
-    return 0
-  if line.startswith('turn on'):
-    return 1
-  if line.startswith('toggle'):
-    return 2
-  return -1
+def parse_line(line):
+  pattern = re.compile(r'(?P<cmd>turn on|turn off|toggle)\s(?P<x1>\d+),(?P<y1>\d+)\sthrough\s(?P<x2>\d+),(?P<y2>\d+)')
+  match = pattern.match(line)
+  command = match.group('cmd')
+  x1 = int(match.group('x1'))
+  y1 = int(match.group('y1'))
+  x2 = int(match.group('x2'))
+  y2 = int(match.group('y2'))
+  return (command, x1, y1, x2, y2)
 
-def get_start(line):
-  words = line.split(',')
-  x = words[0].split(' ')[-1]
-  y = words[1].split(' ')[0]
-  return int(x), int(y)
+def grid_iter(lines):
+  for line in lines:
+    (command, x1, y1, x2, y2) = parse_line(line)
+    for x in range(x1, x2+1):
+      for y in range(y1, y2+1):
+        yield (command, x, y)
 
-def get_end(line):
-  words = line.split(',')
-  x = words[1].split(' ')[-1]
-  y = words[2].split(' ')[0]
-  return int(x), int(y)
+def grid_sum(grid):
+  grid_sum = 0
+  for row in grid:
+    grid_sum += sum(row)
+  return grid_sum
 
 def set_lights1(lines):
   light_grid = [[0 for x in range(1000)] for x in range(1000)]
-  for line in lines:
-    command = get_command(line)
-    startx, starty = get_start(line)
-    endx, endy = get_end(line)
-    for x in range(startx, endx+1):
-      for y in range(starty, endy+1):
-        if command == 0:
-          light_grid[x][y] = 0
-        if command == 1:
-          light_grid[x][y] = 1
-        if command == 2:
-          light_grid[x][y] = 1 - light_grid[x][y]
-  light_count = 0
-  for i in light_grid:
-    for j in i:
-      if j > 0:
-        light_count += 1
-  return light_count
+  for (command, x, y) in grid_iter(lines):
+    if command == 'turn off':
+      light_grid[x][y] = 0
+    if command == 'turn on':
+      light_grid[x][y] = 1
+    if command == 'toggle':
+      light_grid[x][y] = 1 - light_grid[x][y]
+  return grid_sum(light_grid)
 
 def set_lights2(lines):
   light_grid = [[0 for x in range(1000)] for x in range(1000)]
-  for line in lines:
-    command = get_command(line)
-    startx, starty = get_start(line)
-    endx, endy = get_end(line)
-    for x in range(startx, endx+1):
-      for y in range(starty, endy+1):
-        if command == 0 and light_grid[x][y] > 0:
-          light_grid[x][y] -= 1
-        if command == 1:
-          light_grid[x][y] += 1
-        if command == 2:
-          light_grid[x][y] += 2
-  brightness = 0
-  for i in light_grid:
-    for j in i:
-      brightness += j
-  return brightness
+  for (command, x, y) in grid_iter(lines):
+    if command == 'turn off' and light_grid[x][y] > 0:
+      light_grid[x][y] -= 1
+    if command == 'turn on':
+      light_grid[x][y] += 1
+    if command == 'toggle':
+      light_grid[x][y] += 2
+  return grid_sum(light_grid)
 
 if __name__ == "__main__":
   lines = [x.rstrip() for x in fileinput.input()]
--- test_day6.py
+++ test_day6.py
@@ -3,9 +3,27 @@
 import day6
 import unittest
 
-class TestDay5(unittest.TestCase):
+class TestDay6(unittest.TestCase):
+  def test_parse_line(self):
+    self.assertEqual(day6.parse_line('turn on 0,0 through 999,999'),
+      ('turn on', 0, 0, 999, 999))
+    self.assertEqual(day6.parse_line('toggle 249,249 through 250,250'),
+      ('toggle', 249, 249, 250, 250))
+    self.assertEqual(day6.parse_line('turn off 250,250 through 749,749'),
+      ('turn off', 250, 250, 749, 749))
+
   def test_set_lights1(self):
     self.assertEqual(day6.set_lights1(['turn on 0,0 through 999,999']), 1000000)
+    self.assertEqual(day6.set_lights1(['turn on 0,0 through 999,999',
+      'toggle 0,0 through 999,0']), 999000)
+    self.assertEqual(day6.set_lights1(['turn on 0,0 through 499,499',
+      'toggle 249,249 through 250,250']), 249996)
+    self.assertEqual(day6.set_lights1(['turn on 0,0 through 99,99',
+      'turn off 25,25 through 74,74']), 7500)
+
+  def test_set_lights2(self):
+    self.assertEqual(day6.set_lights2(['turn on 0,0 through 0,0']), 1)
+    self.assertEqual(day6.set_lights2(['toggle 0,0 through 999,999']), 2000000)
 
 if __name__ == '__main__':
   unittest.main()
