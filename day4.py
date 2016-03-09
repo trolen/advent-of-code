2016ea16cac54e1c8521ded2b8ec01da32b6b71f
Tim Rolen <trolen@windrock.com>
2016-03-09 16:02:50 +0000

    Change find_hash() to return the actual hash string Output additional information, including hash string Fixed tests

--- day4.py
+++ day4.py
@@ -4,15 +4,24 @@ import hashlib
 
 def find_hash(key, prefix):
   key_hash = hashlib.md5(key.encode())
-  i = 0
+  num = 0
   while True:
     new_hash = key_hash.copy()
-    new_hash.update(str(i).encode())
-    if new_hash.hexdigest().startswith(prefix):
-      return i
-    i += 1
+    new_hash.update(str(num).encode())
+    hash_string = new_hash.hexdigest()
+    if hash_string.startswith(prefix):
+      return (num, hash_string)
+    num += 1
+  return None
 
 if __name__ == '__main__':
   secret_key = 'iwrupvqb'
-  print('Produces "00000" hash: %s' % find_hash(secret_key, '00000'))
-  print('Produces "000000" hash: %s' % find_hash(secret_key, '000000'))
+  print('Secret key: %s' % secret_key)
+  prefix = '00000'
+  (num, hash_string) = find_hash(secret_key, prefix)
+  print('For prefix "%s", found %s.' % (prefix, num))
+  print('Resulting hash: %s' % hash_string)
+  prefix = '000000'
+  (num, hash_string) = find_hash(secret_key, prefix)
+  print('For prefix "%s", found %s.' % (prefix, num))
+  print('Resulting hash: %s' % hash_string)
--- test_day4.py
+++ test_day4.py
@@ -5,8 +5,8 @@ import unittest
 
 class TestDay4(unittest.TestCase):
   def test_find_hash(self):
-    self.assertEqual(day4.find_hash('abcdef', '00000'), 609043)
-    self.assertEqual(day4.find_hash('pqrstuv', '00000'), 1048970)
+    self.assertEqual(day4.find_hash('abcdef', '00000'), (609043, '000001dbbfa3a5c83a2d506429c7b00e'))
+    self.assertEqual(day4.find_hash('pqrstuv', '00000'), (1048970,'000006136ef2ff3b291c85725f17325c'))
 
 if __name__ == '__main__':
   unittest.main()
