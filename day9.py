35b417e6c6c51326a2db6911de20c4db520cda1e
Tim Rolen <trolen@windrock.com>
2016-03-11 01:07:28 +0000

    Change to python3 Fix print statements for python3 Take file input on command line Pull code into class

--- day9.py
+++ day9.py
@@ -1,54 +1,43 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
+import fileinput
 import itertools
 
-INPUT_FILE="day9_input.txt"
-
-all_cities = []
-distances = []
-
-def add_city(city):
-  global all_cities
-  for c in all_cities:
-    if c == city:
-      return
-  all_cities.append(city)
-
-def read_distances():
-  global all_cities
-  global distances
-  with open(INPUT_FILE, 'r') as f:
-    lines = f.readlines()
+class Routes:
+  def __init__(self, lines):
+    self._cities = []
+    self._distances = []
     for line in lines:
-      line = line.rstrip().split(' = ')
+      line = line.split(' = ')
       cities = line[0].split(' to ')
-      add_city(cities[0])
-      add_city(cities[1])
-      distances.append([cities[0], cities[1], int(line[1])])
-      distances.append([cities[1], cities[0], int(line[1])])
-  all_cities.sort()
+      self._add_city(cities[0])
+      self._add_city(cities[1])
+      self._distances.append([cities[0], cities[1], int(line[1])])
+      self._distances.append([cities[1], cities[0], int(line[1])])
+    self._cities.sort()
+    self._routes = list(itertools.permutations(self._cities, len(self._cities)))
 
-def build_routes(cities):
-  return list(itertools.permutations(cities, len(cities)))
+  def _add_city(self, city):
+    if not city in self._cities:
+      self._cities.append(city)
 
-def get_lengths(routes):
-  global distances
-  lengths = []
-  for route in routes:
-    prev_city = ''
-    length = 0
-    for city in route:
-      for dist in distances:
-        if dist[0] == prev_city and dist[1] == city:
-          length += dist[2]
-      prev_city = city
-    lengths.append(length)
-  lengths.sort()
-  return lengths
+  def get_lengths(self):
+    lengths = []
+    for route in self._routes:
+      prev_city = ''
+      length = 0
+      for city in route:
+        for dist in self._distances:
+          if dist[0] == prev_city and dist[1] == city:
+            length += dist[2]
+        prev_city = city
+      lengths.append(length)
+    lengths.sort()
+    return lengths
 
 if __name__ == "__main__":
-  read_distances()
-  routes = build_routes(all_cities)
-  lengths = get_lengths(routes)
-  print "Min length:", lengths[0]
-  print "Max length:", lengths[-1]
+  lines = [line.rstrip() for line in fileinput.input()]
+  routes = Routes(lines)
+  lengths = routes.get_lengths()
+  print("Min route length: %s" % lengths[0])
+  print("Max route length: %s" % lengths[-1])
