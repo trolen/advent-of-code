7542dbf60854f0d58046cb0e21f43d32e7049be9
Tim Rolen <trolen@windrock.com>
2016-03-15 01:56:28 +0000

    Add Day 13

---
+++ day13.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import fileinput
+import itertools
+
+guests = []
+happiness = []
+
+def parse_input(lines):
+  global guests
+  global happiness
+  for line in lines:
+    words = line.split(' ')
+    if not words[0] in guests:
+      guests.append(words[0])
+    sign = 1 if words[2] == 'gain' else -1
+    happiness.append([words[0], words[-1][:-1], sign * int(words[3])])
+
+def find_happiness(g1, g2):
+  global happiness
+  for h in happiness:
+    if h[0] == g1 and h[1] == g2:
+      return h[2]
+  return None
+
+def calc_happiness():
+  global guests
+  perms = list(itertools.permutations(guests))
+  results = []
+  for p in perms:
+    h = 0
+    l = len(p)
+    for i in range(l):
+      h += find_happiness(p[i], p[i - 1])
+      if i < l - 1:
+        h += find_happiness(p[i], p[i + 1])
+      else:
+        h += find_happiness(p[i], p[0])
+    results.append(h)
+  results.sort()
+  return results[-1]
+
+def add_me():
+  global guests
+  global happiness
+  for g in guests:
+    happiness.append(["Me", g, 0])
+    happiness.append([g, "Me", 0])
+  guests.append("Me")
+
+if __name__ == "__main__":
+  lines = [line.rstrip() for line in fileinput.input()]
+  parse_input(lines)
+  print("Happiness: %s" % calc_happiness())
+  add_me()
+  print("Happiness (with me): %s" % calc_happiness())
---
+++ test_day13.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import day13
+import unittest
+
+class TestDay13(unittest.TestCase):
+  def test_(self):
+    lines = ['Alice would gain 54 happiness units by sitting next to Bob.',
+             'Alice would lose 79 happiness units by sitting next to Carol.',
+             'Alice would lose 2 happiness units by sitting next to David.',
+             'Bob would gain 83 happiness units by sitting next to Alice.',
+             'Bob would lose 7 happiness units by sitting next to Carol.',
+             'Bob would lose 63 happiness units by sitting next to David.',
+             'Carol would lose 62 happiness units by sitting next to Alice.',
+             'Carol would gain 60 happiness units by sitting next to Bob.',
+             'Carol would gain 55 happiness units by sitting next to David.',
+             'David would gain 46 happiness units by sitting next to Alice.',
+             'David would lose 7 happiness units by sitting next to Bob.',
+             'David would gain 41 happiness units by sitting next to Carol.']
+    day13.parse_input(lines)
+    self.assertEqual(day13.calc_happiness(), 330)
+
+if __name__ == '__main__':
+  unittest.main()
