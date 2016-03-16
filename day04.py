8e86227281652971f5dd803f1ac8a098b9c7be49
Tim Rolen <trolen@windrock.com>
2016-03-16 14:10:49 +0000

    Attempt to speed up program by checking bytes of digest for zeros instead of comparing string. Not sure it made a difference.

--- day04.py
+++ day04.py
@@ -3,16 +3,24 @@
 import hashlib
 import time
 
+def starts_with_zeros(digest, num_zeros):
+  i = 0
+  while num_zeros > 0:
+    c = digest[i] if num_zeros >= 2 else digest[i] & 0xf0
+    if c != 0:
+      return False
+    i += 1
+    num_zeros -= 2
+  return True
+
 def find_hash(prefix, num_zeros):
   prefix_hash = hashlib.md5(prefix.encode())
-  zeros = '0' * num_zeros
   num = 0
   while True:
     new_hash = prefix_hash.copy()
     new_hash.update(str(num).encode())
-    hexdigest = new_hash.hexdigest()
-    if hexdigest.startswith(zeros):
-      return num, hexdigest
+    if starts_with_zeros(new_hash.digest(), num_zeros):
+      return num, new_hash.hexdigest()
     num += 1
   return None
 
