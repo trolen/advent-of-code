445139c66dcbe579e22e9c958a40c9a152e8c31f
Tim Rolen <trolen@windrock.com>
2016-03-16 02:19:55 +0000

    Added timing

--- day04.py
+++ day04.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import hashlib
+import time
 
 def find_hash(key, prefix):
   key_hash = hashlib.md5(key.encode())
@@ -10,18 +11,19 @@ def find_hash(key, prefix):
     new_hash.update(str(num).encode())
     hash_string = new_hash.hexdigest()
     if hash_string.startswith(prefix):
-      return (num, hash_string)
+      return num, hash_string
     num += 1
   return None
 
 if __name__ == '__main__':
   secret_key = 'iwrupvqb'
-  print('Secret key: %s' % secret_key)
-  prefix = '00000'
-  (num, hash_string) = find_hash(secret_key, prefix)
-  print('For prefix "%s", found %s.' % (prefix, num))
-  print('Resulting hash: %s' % hash_string)
-  prefix = '000000'
-  (num, hash_string) = find_hash(secret_key, prefix)
-  print('For prefix "%s", found %s.' % (prefix, num))
-  print('Resulting hash: %s' % hash_string)
+  t1 = time.time()
+  num, hash_string = find_hash(secret_key, "00000")
+  t2 = time.time()
+  print('For %s%s -> %s' % (secret_key, num, hash_string))
+  print(' Time: %s sec' % (t2 - t1))
+  t1 = time.time()
+  num, hash_string = find_hash(secret_key, '000000')
+  t2 = time.time()
+  print('For %s%s -> %s' % (secret_key, num, hash_string))
+  print(' TIme: %s sec' % (t2 - t1))
