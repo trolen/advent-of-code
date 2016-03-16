10b0b468d76b8b88d7556350d2dee10100ace22b
Tim Rolen <trolen@windrock.com>
2016-03-16 13:34:34 +0000

    Change function to take int (number of zeros) instead of string Rename some variables to make meaning clearer Fix unit tests

--- day04.py
+++ day04.py
@@ -3,27 +3,28 @@
 import hashlib
 import time
 
-def find_hash(key, prefix):
-  key_hash = hashlib.md5(key.encode())
+def find_hash(prefix, num_zeros):
+  prefix_hash = hashlib.md5(prefix.encode())
+  zeros = '0' * num_zeros
   num = 0
   while True:
-    new_hash = key_hash.copy()
+    new_hash = prefix_hash.copy()
     new_hash.update(str(num).encode())
-    hash_string = new_hash.hexdigest()
-    if hash_string.startswith(prefix):
-      return num, hash_string
+    hexdigest = new_hash.hexdigest()
+    if hexdigest.startswith(zeros):
+      return num, hexdigest
     num += 1
   return None
 
 if __name__ == '__main__':
-  secret_key = 'iwrupvqb'
+  prefix = 'iwrupvqb'
   t1 = time.time()
-  num, hash_string = find_hash(secret_key, "00000")
+  num, hexdigest = find_hash(prefix, 5)
   t2 = time.time()
-  print('For %s%s -> %s' % (secret_key, num, hash_string))
+  print('For %s%s -> %s' % (prefix, num, hexdigest))
   print(' Time: %s sec' % (t2 - t1))
   t1 = time.time()
-  num, hash_string = find_hash(secret_key, '000000')
+  num, hexdigest = find_hash(prefix, 6)
   t2 = time.time()
-  print('For %s%s -> %s' % (secret_key, num, hash_string))
+  print('For %s%s -> %s' % (prefix, num, hexdigest))
   print(' Time: %s sec' % (t2 - t1))
--- test_day04.py
+++ test_day04.py
@@ -5,8 +5,8 @@ import unittest
 
 class TestDay4(unittest.TestCase):
   def test_find_hash(self):
-    self.assertEqual(day04.find_hash('abcdef', '00000'), (609043, '000001dbbfa3a5c83a2d506429c7b00e'))
-    self.assertEqual(day04.find_hash('pqrstuv', '00000'), (1048970,'000006136ef2ff3b291c85725f17325c'))
+    self.assertEqual(day04.find_hash('abcdef', 5), (609043, '000001dbbfa3a5c83a2d506429c7b00e'))
+    self.assertEqual(day04.find_hash('pqrstuv', 5), (1048970,'000006136ef2ff3b291c85725f17325c'))
 
 if __name__ == '__main__':
   unittest.main()
