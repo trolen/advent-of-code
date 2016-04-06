fc1dbb982d80a76e0b22ce09f162e4f3e0762b07
trolen <trolen@windrock.com>
2016-04-05 21:55:58 -0400

    Rework code to try and prevent memory error (and hopefully finish running)

--- day19/day19.py
+++ day19/day19.py
@@ -18,35 +18,27 @@ class Molecules:
   def _replacement(self, molecule):
     molecules = []
     for repl in self._replacements:
-      n = molecule.count(repl[0])
-      p = 0
-      while n > 0:
-        p = molecule.find(repl[0], p)
-        s = molecule[:p]
-        s += molecule[p:].replace(repl[0], repl[1], 1)
-        molecules.append(s)
-        n -= 1
-        p += len(repl[0])
+      n = len(repl[0])
+      p = molecule.find(repl[0])
+      while p >= 0:
+        molecules.append(molecule[:p] + molecule[p:].replace(repl[0], repl[1], 1))
+        p = molecule.find(repl[0], p + n)
     return list(set(molecules))
 
   def calibrate(self, molecule):
     return len(self._replacement(molecule))
 
   def fabricate(self, result):
-    count = 1
-    molecules = self._replacement('e')
-    while len(molecules) > 0 and result not in molecules:
-      new_molecules = []
-      for molecule in molecules:
-        new_molecules += self._replacement(molecule)
-      for molecule in new_molecules:
-        if len(molecule) > len(result):
-          new_molecules.remove(molecule)
-      molecules = list(set(new_molecules))
-      count += 1
-      print(count, len(molecules))
-    return count
-        
+    molecules = [(1,x) for x in self._replacement('e')]
+    while len(molecules) > 0:
+      m = molecules.pop(0)
+      n = m[0] + 1
+      for x in self._replacement(m[1]):
+        if x == result:
+          return n
+        if len(x) < len(result):
+          molecules.insert(0, (n, x))
+
 def read_input():
   if len(sys.argv) < 2:
     print("Input file name missing")
