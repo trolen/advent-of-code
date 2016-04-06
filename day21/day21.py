18e46e3480fafc78f4ba3c455f29a61e7981ff8e
trolen <trolen@windrock.com>
2016-04-05 22:00:24 -0400

    Begin Day 21

---
+++ day21/day21.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import fileinput
+import sys
+
+def read_input():
+  if len(sys.argv) < 2:
+    print("Input file name missing")
+    sys.exit(1)
+  return [line.rstrip() for line in fileinput.input()]
+
+if __name__ == "__main__":
+  lines = read_input()
---
+++ day21/test_day21.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import day21
+import unittest
+
+class TestDay21(unittest.TestCase):
+  def test_1(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
