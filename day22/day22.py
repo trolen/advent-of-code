a2da1c3fe25acde9101028eb8b7519f21ae03e54
trolen <timothy.rolen@gmail.com>
2017-09-05 06:56:55 -0400

    Consolidate classes to Player. Flesh out game play code.

--- day22/day22.py
+++ day22/day22.py
@@ -1,55 +1,60 @@
 #!/usr/bin/env python3
 
-class Wizard:
-  def __init__(self, hit_points, mana):
-    self.reset(hit_points, mana)
+class Player:
+    def __init__(self, hit_points, armor, mana, damage):
+        self.reset(hit_points, armor, mana, damage)
 
-  def reset(self, hit_points, mana):
-    self.hit_points = hit_points
-    self.mana = mana
-    self.armor = 0
+    def reset(self, hit_points, armor, mana, damage):
+        self.hit_points = hit_points
+        self.armor = armor
+        self.mana = mana
+        self.damage = damage
 
-class Boss:
-  def __init__(self, hit_points, damage):
-    self.reset(hit_points, damage)
-
-  def reset(self, hit_points, damage):
-    self.hit_points = hit_points
-    self.damage = damage
-    self.armor = 0
 
 class Spell:
-  def __init__(self, name, cost, effect, damage, heal, armor, mana):
-    self.name = name
-    self.cost = cost
-    self.effect = effect
-    self.damage = damage
-    self.heal = heal
-    self.armor = armor
-    self.mana = mana
-    self.timer = 0
+    def __init__(self, name, cost, effect, damage, heal, armor, mana):
+        self.name = name
+        self.cost = cost
+        self.effect = effect
+        self.damage = damage
+        self.heal = heal
+        self.armor = armor
+        self.mana = mana
+        self.timer = 0
 
-  def buy(self, player, boss):
-      player.mana -= self.cost
-      if self.effect == 0:
-          if self.damage > 0:
-              boss.hit_points -= self.damage
-          if self.heal > 0:
-              player.hit_points += self.heal
+    def can_cast(self, wizard):
+        if wizard.mana < self.cost:
+            return False
+        if self.timer > 0:
+            return False
+        return True
 
-  def apply_effect(self, player, boss):
-      if self.effect > 0 and self.timer > 0:
-          if self.damage > 0:
-              boss.hit_points -= spell.damage
-          if self.armor > 0:
-              player.armor = self.armor
-          if self.mana > 0:
-              player.mana += spell.mana
-          self.timer -= 1
+    def cast(self, wizard, boss):
+        if not self.can_cast(wizard):
+            return
+        wizard.mana -= self.cost
+        if self.effect > 0:
+            self.timer = self.effect
+        else:
+            if self.damage > 0:
+                boss.hit_points -= self.damage
+            if self.heal > 0:
+                wizard.hit_points += self.heal
 
-  def cancel_effect(self, player, boss):
-      if self.armor > 0 and self.timer <= 0:
-          player.armor = 0
+    def apply_effect(self, wizard, boss):
+        if self.effect > 0 and self.timer > 0:
+            if self.damage > 0:
+                boss.hit_points -= self.damage
+            if self.armor > 0:
+                wizard.armor = self.armor
+            if self.mana > 0:
+                wizard.mana += self.mana
+            self.timer -= 1
+
+    def cancel_armor_effect(self, wizard):
+        if self.armor > 0 and self.timer <= 0:
+            wizard.armor = 0
+
 
 SPELLS = [Spell('Magic Missle', 53, 0, 4, 0, 0, 0),
           Spell('Drain', 73, 0, 2, 2, 0, 0),
@@ -57,6 +62,40 @@ SPELLS = [Spell('Magic Missle', 53, 0, 4, 0, 0, 0),
           Spell('Poison', 173, 6, 3, 0, 0, 0),
           Spell('Recharge', 229, 5, 0, 0, 0, 101)]
 
+
+def apply_effects(wizard, boss):
+    for spell in SPELLS:
+        spell.apply_effect(wizard, boss)
+
+
+def cancel_effects(wizard):
+    for spell in SPELLS:
+        spell.cancel_armor_effect(wizard)
+
+
+def wizard_turn(wizard, boss, spell):
+    apply_effects(wizard, boss)
+    if boss.hit_points <= 0:
+        return True
+    cancel_effects(wizard)
+    if spell.can_cast(wizard):
+        spell.cast(wizard, boss)
+    return boss.hit_points <= 0
+
+
+def boss_turn(wizard, boss):
+    apply_effects(wizard, boss)
+    if boss.hit_points <= 0:
+        return False
+    wizard.hit_points -= max(1, boss.damage - wizard.armor)
+    cancel_effects(wizard)
+    return wizard.hit_points <= 0
+
+
+def play_game(wizard, boss):
+    pass
+
+
 if __name__ == "__main__":
-    player = Wizard(50, 500)
-    boss = Boss(55, 8)
+    wizard = Player(50, 0, 500, 0)
+    boss = Player(55, 0, 0, 8)
--- day22/test_day22.py
+++ day22/test_day22.py
@@ -5,8 +5,9 @@ import unittest
 
 class TestDay22(unittest.TestCase):
     def test_play_game(self):
-        player = day22.Wizard(10, 250)
-        boss = day22.Boss(13, 8)
+        wizard = day22.Wizard(10, 0, 250)
+        boss = day22.Boss(13, 0, 8)
+        self.assertTrue(day22.play_game(wizard, boss))
 
 if __name__ == "__main__":
     unittest.main()
