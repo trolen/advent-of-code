3c4568ee355bee61c0a8d85a7e179abbef4de220
trolen <timothy.rolen@gmail.com>
2017-10-07 17:05:33 -0400

    Make SPELLS a dictionary

--- 2015/day22/day22.py
+++ 2015/day22/day22.py
@@ -2,8 +2,7 @@
 
 
 class Spell:
-    def __init__(self, name, cost, duration, damage, healing, armor, mana):
-        self.name = name
+    def __init__(self, cost, duration, damage, healing, armor, mana):
         self.cost = cost
         self.duration = duration
         self.damage = damage
@@ -12,13 +11,13 @@ class Spell:
         self.mana = mana
 
 
-SPELLS = [
-    Spell('Magic Missle', 53, 0, 4, 0, 0, 0),
-    Spell('Drain', 73, 0, 2, 2, 0, 0),
-    Spell('Shield', 113, 6, 0, 0, 7, 0),
-    Spell('Poison', 173, 6, 3, 0, 0, 0),
-    Spell('Recharge', 229, 5, 0, 0, 0, 101)
-]
+SPELLS = {
+    'Magic Missle': Spell(53, 0, 4, 0, 0, 0),
+    'Drain': Spell(73, 0, 2, 2, 0, 0),
+    'Shield': Spell(113, 6, 0, 0, 7, 0),
+    'Poison': Spell(173, 6, 3, 0, 0, 0),
+    'Recharge': Spell(229, 5, 0, 0, 0, 101)
+}
 
 
 class Player:
@@ -31,12 +30,13 @@ class Player:
         return Player(self.hit_points, self.mana, self.armor)
 
     def can_cast_spells(self, timers):
-        for spell in SPELLS:
+        for key in SPELLS:
+            spell = SPELLS[key]
             if self.mana < spell.cost:
                 continue
-            if spell.name in timers:
+            if key in timers:
                 continue
-            yield spell
+            yield key
 
 
 class Boss:
@@ -48,32 +48,26 @@ class Boss:
         return Boss(self.hit_points, self.damage)
 
 
-def find_spell(name):
-    for spell in SPELLS:
-        if spell.name == name:
-            return spell
-    return None
-
-
 def play_game(player, boss, timers={}):
     def apply_effects():
-        keys = [k for k in timers.keys()]
-        for timer in keys:
-            spell = find_spell(timer)
+        keys = [timer for timer in timers]
+        for key in keys:
+            spell = SPELLS[key]
             boss.hit_points -= spell.damage
             if spell.armor > 0:
                 player.armor = spell.armor
             player.mana += spell.mana
-            timers[timer] -= 1
-            if timers[timer] == 0:
+            timers[key] -= 1
+            if timers[key] == 0:
                 if spell.armor > 0:
                     player.armor = 0
-                del timers[timer]
+                del timers[key]
 
-    def cast_spell(spell):
+    def cast_spell(key):
+        spell = SPELLS[key]
         player.mana -= spell.cost
         if spell.duration > 0:
-            timers[spell.name] = spell.duration
+            timers[key] = spell.duration
         else:
             boss.hit_points -= spell.damage
             player.hit_points += spell.healing
@@ -84,12 +78,12 @@ def play_game(player, boss, timers={}):
     saved_player = player.copy()
     saved_boss = boss.copy()
     saved_timers = timers.copy()
-    player_can_cast_spells = [s for s in player.can_cast_spells(timers)]
-    for spell in player_can_cast_spells:
+    for key in saved_player.can_cast_spells(timers):
+        spell = SPELLS[key]
         player = saved_player.copy()
         boss =  saved_boss.copy()
         timers = saved_timers.copy()
-        cast_spell(spell)
+        cast_spell(key)
         if boss.hit_points <= 0:
             return True, spell.cost
         apply_effects()
