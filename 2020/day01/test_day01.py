81781bfa93e9d97bb363695899aa043f0e96b929
Timothy Rolen <timothy.rolen@gmail.com>
2020-12-07 12:05:43 -0500

    Move data initialization into setUp() method.

--- 2020/day01/test_day01.py
+++ 2020/day01/test_day01.py
@@ -5,13 +5,14 @@ import day01
 
 
 class TestDay01(unittest.TestCase):
+    def setUp(self):
+        self.entries = [1721, 979, 366, 299, 675, 1456]
+
     def test_part1(self):
-        entries = [1721, 979, 366, 299, 675, 1456]
-        self.assertEqual(514579, day01.do_part1(entries))
+        self.assertEqual(514579, day01.do_part1(self.entries))
 
     def test_part2(self):
-        entries = [1721, 979, 366, 299, 675, 1456]
-        self.assertEqual(241861950, day01.do_part2(entries))
+        self.assertEqual(241861950, day01.do_part2(self.entries))
 
 if __name__ == '__main__':
     unittest.main()
