af10e093a5541a619cee5e6171d6e7624de062fb
Timothy Rolen <timothy.rolen@gmail.com>
2022-10-23 10:41:09 -0400

    Finish part one

--- 2016/day11/day11.py
+++ 2016/day11/day11.py
@@ -1,39 +1,9 @@
 #! /usr/bin/env python3
 
-class Application:
-    def __init__(self, raw_data):
-        self._floor_grid = self._parse_data(raw_data)
-        self._elevator = 0
+import re
 
-    def _parse_data(self, raw_data):
-        result = []
-        for line in raw_data:
-            floor = []
-            terms = line.split(',')
-            for term in terms:
-                items = term.split(' and ')
-                for item in items:
-                    if len(item) == 0:
-                        continue
-                    words = item.split(' ')
-                    element = words[-2]
-                    if element == 'nothing':
-                        continue
-                    element = element[:2]
-                    type = words[-1][0]
-                    floor.append((element, type))
-            result.append(floor)
-        print(result)
-        return result
-
-    def _finished(self):
-        for i in range(0, len(self._floor_grid) - 1):
-            if len(self._floor_grid[i]) > 0:
-                return False
-        return True
-
-    def execute(self):
-        pass
+from collections import Counter, deque
+from itertools import chain, combinations
 
 
 def read_data(filename):
@@ -41,7 +11,59 @@ def read_data(filename):
         return [line.strip() for line in file]
 
 
+def parse_input(lines):
+    return [set(re.findall(r'(\w+)(?:-compatible)? (microchip|generator)', line)) for line in lines]
+
+
+def is_valid_floor(floor):
+    return len(set(t for _, t in floor)) < 2 or \
+           all((obj, 'generator') in floor for (obj, t) in floor if t == 'microchip')
+
+
+def next_states(state):
+    moves, elevator, floors = state
+    possible_moves = chain(combinations(floors[elevator], 2), combinations(floors[elevator], 1))
+    for move in possible_moves:
+        for direction in [-1, 1]:
+            next_elevator = elevator + direction
+            if not 0 <= next_elevator < len(floors):
+                continue
+            next_floors = floors.copy()
+            next_floors[elevator] = next_floors[elevator].difference(move)
+            next_floors[next_elevator] = next_floors[next_elevator].union(move)
+            if is_valid_floor(next_floors[elevator]) and is_valid_floor(next_floors[next_elevator]):
+                yield moves + 1, next_elevator, next_floors
+
+
+def is_all_top_level(floors):
+    return all(not floor for number, floor in enumerate(floors) if number < len(floors) - 1)
+
+
+def count_floor_objects(state):
+    _, elevator, floors = state
+    return elevator, tuple(tuple(Counter(type for _, type in floor).most_common()) for floor in floors)
+
+
+def min_moves_to_top_level(floors):
+    seen = set()
+    queue = deque([(0, 0, floors)])
+    while queue:
+        state = queue.popleft()
+        moves, _, floors = state
+        if is_all_top_level(floors):
+            return moves
+        for next_state in next_states(state):
+            if (key := count_floor_objects(next_state)) not in seen:
+                seen.add(key)
+                queue.append(next_state)
+
+
+def do_part1(data):
+    floors = parse_input(data)
+    return min_moves_to_top_level(floors)
+
+
 if __name__ == '__main__':
-    raw_data = read_data('input.txt')
-    app = Application(raw_data)
-    app.execute()
+    rawData = read_data('input.txt')
+    result1 = do_part1(rawData)
+    print('Part 1 result:', result1)
--- 2016/day11/test_day11.py
+++ 2016/day11/test_day11.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
 
+
 import unittest
 import day11
 
 class TestDay11(unittest.TestCase):
     def setUp(self):
-        self.raw_data = [
+        self._data = [
             'The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.',
             'The second floor contains a hydrogen generator.',
             'The third floor contains a lithium generator.',
@@ -13,7 +14,6 @@ class TestDay11(unittest.TestCase):
         ]
 
     def test_part1(self):
-        app = day11.Application(self.raw_data)
         pass
 
 
