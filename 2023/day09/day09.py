61ce04b6660b9471382c79d047006407da07d9dd
Timothy Rolen <timothy.rolen@gmail.com>
2024-09-28 15:39:11 -0400

    Add day09

---
+++ 2023/day09/day09.py
@@ -0,0 +1,36 @@
+
+def read_input(filename):
+    with open(filename) as file:
+        return [line.strip() for line in file.readlines()]
+
+
+class Application:
+    def __init__(self, raw_input):
+        self._parse_input(raw_input)
+
+    def _parse_input(self, raw_input):
+        self._data = [[int(x) for x in line.split(' ')] for line in raw_input]
+
+    def _find_next(self, numbers):
+        if all(n == 0 for n in numbers):
+            return 0
+        differences = [(numbers[i] - numbers[i - 1]) for i in range(1, len(numbers))]
+        return numbers[-1] + self._find_next(differences)
+
+
+    def do_part1(self):
+        return sum(self._find_next(row) for row in self._data)
+
+    def do_part2(self):
+        return sum(self._find_next(row[::-1]) for row in self._data)
+
+
+def main():
+    raw_input = read_input('input.txt')
+    app = Application(raw_input)
+    print('Part 1:', app.do_part1())
+    print('Part 2:', app.do_part2())
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2023/day09/test_day09.py
@@ -0,0 +1,21 @@
+import day09
+import unittest
+
+class TestDay08(unittest.TestCase):
+    def setUp(self):
+        raw_input = [
+            '0 3 6 9 12 15',
+            '1 3 6 10 15 21',
+            '10 13 16 21 30 45'
+        ]
+        self._app = day09.Application(raw_input)
+
+    def test_part1(self):
+        self.assertEqual(114, self._app.do_part1())
+
+    def test_part2(self):
+        self.assertEqual(2, self._app.do_part2())
+
+
+if __name__ == '__main__':
+    unittest.main()
